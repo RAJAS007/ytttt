528025ab8db393140e727cfdde0f6d8a187fc1ee
RAJAS007 <143735397+RAJAS007@users.noreply.github.com>
2025-12-23 18:23:57 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// All routes go to index.html (for SPA support)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+  console.log(`Open http://localhost:${port} in your browser`);
+});
